81e44cfacd12eb94caf73fdd30cea016f15f0617
PotlaVani2002 <vpotla@msa.miraclesoft.com>
2026-01-19 17:24:14 +0530

    Login Form

--- Forms-app/src/app/login-form/login-form.component.ts
+++ Forms-app/src/app/login-form/login-form.component.ts
@@ -1,30 +1,45 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
 
-  userForms:FormGroup=new FormGroup({
-    age:new FormControl('0'),
-    name:new FormControl(''),
-    address: new FormGroup({
-      ad1:new FormControl(''),
-      ad2:new FormControl(''),
-      state:new FormControl(''),
-      city:new FormControl(''),
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6)
+    ]),
+    cpassword: new FormControl('', [Validators.required])
+  },
+    {
+      validators: this.passwordChecker
     })
 
-  })
-  
-  saveUser(){
-    // debugger;
-    const obj=this.userForms.value;
-    console.log(obj)
+
+  passwordChecker(control: AbstractControl) {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('cpassword')?.value;
+
+    if (!password || !confirmPassword) {
+      return null;
+    }
+
+    return password === confirmPassword
+      ? null
+      : { passwordMismatch: true };
+  }
+
+  submitForm() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
   }
 }
