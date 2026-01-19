369186c691307d28d0504031b3f56d8f4caae796
Vani Potla <115637815+PotlaVani2002@users.noreply.github.com>
2026-01-19 14:26:40 +0530

    Update Palindrome.py

--- Python/Palindrome.py
+++ Python/Palindrome.py
@@ -1,6 +1,12 @@
 #write a function that takes a string and returns True if its a palindrome else false.
+def palindrome(word):
+    if word.lower() in word[::-1].lower():
+       return True
+    else:
+        return False
+
 word="Mam"
-if word.lower() in word[::-1].lower():
+if palindrome(word):
     print("Palindrome")
 else:
     print("Not Palindrome")
