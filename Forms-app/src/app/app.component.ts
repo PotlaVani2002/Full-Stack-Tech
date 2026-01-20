355e21016696859d3375b9ca43286498b84e9390
PotlaVani2002 <vpotla@msa.miraclesoft.com>
2026-01-20 23:49:54 +0530

    Event binding Example

--- Forms-app/src/app/app.component.ts
+++ Forms-app/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { LoginFormComponent } from './login-form/login-form.component';
 import { NestedformComponent } from './nestedform/nestedform.component';
 import { StringintepolationComponent } from './stringintepolation/stringintepolation.component';
 import { PropertybindingComponent } from './propertybinding/propertybinding.component';
+import { EventbindingComponent } from './eventbinding/eventbinding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ReactiveFormsModule,FormsModule,LoginFormComponent,NestedformComponent,StringintepolationComponent,PropertybindingComponent],
+  imports: [RouterOutlet,ReactiveFormsModule,FormsModule,LoginFormComponent,NestedformComponent,StringintepolationComponent,PropertybindingComponent,EventbindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Forms-app/src/app/eventbinding/eventbinding.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eventbinding',
+  standalone: true,
+  imports: [],
+  templateUrl: './eventbinding.component.html',
+  styleUrl: './eventbinding.component.css'
+})
+export class EventbindingComponent {
+  email:string='';
+  onEmailChange(event:any){
+    this.email=event.target.value;
+  }
+}
