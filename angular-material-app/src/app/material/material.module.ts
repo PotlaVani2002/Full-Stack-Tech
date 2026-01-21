38d54ba784de7d272c026a4679bf39bfced22d31
= <potlavani9194@gmail.com>
2026-01-21 12:28:12 +0530

    buttons example

--- angular-material-app/src/app/material/material.module.ts
+++ angular-material-app/src/app/material/material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatButtonModule} from '@angular/material/button';
 
+
 const MaterialComponents=[
   MatButtonModule,MatSlideToggleModule
 ];
