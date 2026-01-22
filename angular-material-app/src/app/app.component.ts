f91b7ed31ed666e43708a9229e5bfc849f7d109a
= <potlavani9194@gmail.com>
2026-01-22 12:37:12 +0530

    Resolved merge conflicts

--- angular-material-app/src/app/app.component.ts
+++ angular-material-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MaterialModule } from './material/material.module';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MaterialModule],
+  imports: [RouterOutlet,MaterialModule,ButtonsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
